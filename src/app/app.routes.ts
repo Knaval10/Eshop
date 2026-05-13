af309a88002b0506b6d79968936a58c2c733b362
Nabal Khadka <nabal.khadka7@gmail.com>
2026-05-13 15:17:06 +0545

    routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about').then((m) => m.About),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./features/products/products').then((m) => m.Products),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout').then((m) => m.Checkout),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
